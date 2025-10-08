de9ddfd6caa2984f694a7e5f50677bdcd64ff3f4
umaiwa1 <mumair.realone@gmail.com>
2025-10-08 12:11:03 +0500

    update file

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ status: 'OK App is Running and workingg' });
+    res.json({ status: 'OK App is Running and workinggg' });
 });
 
 if (require.main === module) {
