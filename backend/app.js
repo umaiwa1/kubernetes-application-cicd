71d550d64a18ee61498cba1a20d608314a7747c0
umaiwa1 <mumair.realone@gmail.com>
2025-10-09 12:35:36 +0500

    update cicd and app file

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ status: process.env.STATUS });
+    res.json({ status: process.env.STATUS_MESSAGE });
 });
 
 if (require.main === module) {
