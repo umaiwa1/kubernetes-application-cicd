15ff11d4571857b28c702eb0cad8036c98d0c721
umaiwa1 <mumair.realone@gmail.com>
2025-10-09 10:53:57 +0500

    update cicd and app file

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ status: 'OK App is Running and working2' });
+    res.json({ status: 'OK App is Running and working3' });
 });
 
 if (require.main === module) {
